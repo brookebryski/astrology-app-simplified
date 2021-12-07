8e860b103bdff78f344bda1f0dad1f291cb1df3f
BrookeBryski <brookeabryski@gmail.com>
2021-12-07 11:06:49 -0600

    initial commit

---
+++ src/main/java/javapractice/astrologyappsimplified/AstrologyAppSimplifiedApplication.java
@@ -0,0 +1,13 @@
+package javapractice.astrologyappsimplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstrologyAppSimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstrologyAppSimplifiedApplication.class, args);
+	}
+
+}
