a876b292fe5fa01d21913771c3cf94f192ebc023
BrookeBryski <brookeabryski@gmail.com>
2021-12-07 13:58:57 -0600

    flow control tests

--- src/test/java/javapractice/astrologyappsimplified/FlowControlTest.java
+++ src/test/java/javapractice/astrologyappsimplified/FlowControlTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class FlowControlTest {
     private IZodiacValidator _zodiacValidator;
@@ -21,9 +21,41 @@ class FlowControlTest {
     @Test
     public void givenAquariusCallIsZodiacOnce() {
         // Given: I am a user
-        // When: I enter the string Aquarius
+        // When: I enter the string "Aquarius"
+        when(_zodiacValidator.isZodiac("aquarius")).thenReturn(true);
+        _flowControl.run("aquarius");
         // Then: Call isZodiac() once
-
+        verify(_zodiacValidator, times(1)).isZodiac("aquarius");
     }
 
+    @Test
+    public void givenAquariusCallZodiacOnce() {
+        // Given: I am a user
+        when(_zodiacValidator.isZodiac("aquarius")).thenReturn(true);
+        when(_zodiac.generateTraits("aquarius")).thenReturn(anyString());
+        _flowControl.run("aquarius");
+        // When: I enter the string "Aquarius"
+        // Then: Call Zodiac() once
+        verify(_zodiac, times(1)).generateTraits("aquarius");
+    }
+
+    @Test
+    public void givenBobCallIsZodiac() {
+        // Given: I am a user
+        // When: I enter the string "Bob"
+        when(_zodiacValidator.isZodiac("bob")).thenReturn(false);
+        _flowControl.run("bob");
+        // Then: Call isZodiac() once
+        verify(_zodiacValidator,times(1)).isZodiac("bob");
+    }
+
+    @Test
+    public void givenBobDoNotCallZodiac() {
+        // Given: I am a user
+        // When: I enter the string "Bob"
+        when(_zodiac.generateTraits(anyString())).thenReturn(anyString());
+        _flowControl.run("bob");
+        // Then: Call Zodiac() zero times
+        verify(_zodiac, never()).generateTraits(anyString());
+    }
 }
